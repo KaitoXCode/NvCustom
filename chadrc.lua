2bbf9a667f6ca604d98fd4bc7c8ee6d8f56a4fcf
KaitoXCode <gerhard.rahn1@gmail.com>
2023-12-04 16:44:38 +0000

    default transparent ui

--- chadrc.lua
+++ chadrc.lua
@@ -7,6 +7,7 @@ local highlights = require "custom.highlights"
 M.ui = {
   theme = "jellybeans",
   theme_toggle = { "jellybeans", "one_light" },
+  transparency = false,
 
   hl_override = highlights.override,
   hl_add = highlights.add,
