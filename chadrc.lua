839b676f78195da05597810a8421ff627339b093
GerhardRahn <gerhard.rahn1@gmail.com>
2024-01-02 09:33:15 +0000

    make trasparency default true

--- chadrc.lua
+++ chadrc.lua
@@ -7,7 +7,7 @@ local highlights = require "custom.highlights"
 M.ui = {
   theme = "jellybeans",
   theme_toggle = { "jellybeans", "one_light" },
-  transparency = false,
+  transparency = true,
 
   hl_override = highlights.override,
   hl_add = highlights.add,
